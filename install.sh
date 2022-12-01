b4a7f939f721588cdcfe08ff5dbeed3c45af7775
20020314 <yitianyangtwo@qq.com>
2022-12-01 08:34:01 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#安装Docker
+curl -fsSL https://get.docker.com | bash
+#安装Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose version
